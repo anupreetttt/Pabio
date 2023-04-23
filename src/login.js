657ee15de104bc929585590a990de0e72a07d5c9
Anupreet Paulkar <apaulk2@uic.edu>
2023-04-23 02:03:40 -0500

    Login page

---
+++ src/login.js
@@ -0,0 +1,40 @@
+import React, { useState } from "react";
+import "./LoginPage.css"; // Import the styles from a separate CSS file
+
+function LoginPage() {
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    // Call your login API endpoint with the entered username and password
+    // You can also perform validation on the username and password fields before calling the API
+  };
+
+  return (
+    <div className="login-page">
+      <h1>Pabio</h1>
+      <form onSubmit={handleSubmit}>
+        <div>
+          <label>Username:</label>
+          <input
+            type="text"
+            value={username}
+            onChange={(event) => setUsername(event.target.value)}
+          />
+        </div>
+        <div>
+          <label>Password:</label>
+          <input
+            type="password"
+            value={password}
+            onChange={(event) => setPassword(event.target.value)}
+          />
+        </div>
+        <button type="submit">Login</button>
+      </form>
+    </div>
+  );
+}
+
+export default LoginPage;
